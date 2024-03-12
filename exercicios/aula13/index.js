0dbcdda800351071567bef9641136added4dc57c
Dhonata Chaves <153765744+DhonataChaves@users.noreply.github.com>
2024-03-12 10:35:36 -0300

    funções math

---
+++ exercicios/aula13/index.js
@@ -0,0 +1,23 @@
+//Objeto math
+let num1 = 25;
+console.log(num1 ** 0.5);
+
+
+
+//let num2 = Math.floor(num1); arredonda o número para baixo
+
+//let num2 = Math.ceil(num1); arredonda o número para cima
+
+//let num2 = Math.round(num1); arredonda para o número mais próximo
+
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); retorna o maior número encontrado em uma sequência
+
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); retorna o menor número encontrado em uma sequência
+
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5); (Math.random = retorna numeros aleatorios que e é personalizavel com parametros)
+
+//console.log(Math.PI) retorna o valor de PI
+
+//console.log(Math.pow(2, 10)); realiza a potenciação
+
+
