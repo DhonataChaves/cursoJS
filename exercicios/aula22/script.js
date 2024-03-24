a2608108bf3e8af3bb382ea399c6803867c38e55
Dhonata Chaves <153765744+DhonataChaves@users.noreply.github.com>
2024-03-23 22:26:52 -0300

    exercicio avaliação de curto circuito(short-circuit)

---
+++ exercicios/aula22/script.js
@@ -0,0 +1,17 @@
+/*
+AVALIAÇÃO DE CURTO-CIRCUITO (SHORT-CIRCUIT)
+
+&& -> false && true -> false (a partir do momento que encontra a expressão falsa ela para a validação e te retorna o "valor falso")
+|| -> true || false -> (a partir do momento que encontra a expressão correta ela para a validação e te retorna o "valor verdeiro")
+
+valores que avalião em falso
+FALSY
+*false
+0
+'' "" ``
+null / undefined
+Nan
+*/
+
+console.log('dhonata' || null || "" || 'Acertou'); 
+
